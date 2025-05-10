36243ba29459a0651b5058d94d9c1fe7693e5ca7
Sandeep Sahani <sylveryte@protonmail.com>
2025-05-10 12:36:49 +0530

    Minimal markdown parser grammar added

---
+++ example.md
@@ -0,0 +1,12 @@
+# hello
+
+- this is a list
+- this is [[example]]
+
+hello
+
+## Second
+
+[[example|some other]]
+
+SOme #thistag pragatr [[example#hello]]
--- grammar.js
+++ grammar.js
@@ -1,17 +1,43 @@
 /**
- * @file Markdown parser for sylmark lsp
+ * @file Minimal Markdown parser for sylmark an markdown pkm lsp
  * @author Sandeep Sahani <sylveryte@protonmail.com>
  * @license MIT
  */
 
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
-
 module.exports = grammar({
   name: "sylmark",
 
+  extras: ($) => [/\s+/],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
-  }
+    document: ($) =>
+      repeat(choice($.heading, $.link, $.wikilink, $.tag, $.text)),
+
+    heading: () =>
+      seq(
+        /[#]{1,6}/,
+        optional(/\s+/),
+        field("title", /[^\n\r]*/),
+        optional("\n"),
+      ),
+
+    link: () =>
+      seq(
+        "[",
+        field("text", repeat(choice(/[^\]]/, "\\]"))),
+        "]",
+        "(",
+        field("url", repeat(choice(/[^)\n]/, choice("\\)", "\n")))),
+        ")",
+      ),
+
+    wikilink: () =>
+      seq("[[", field("target", repeat(choice(/[^]\n]/, "\\]"))), "]]"),
+
+    tag: () => token(seq("#", /[a-zA-Z0-9_]+/)),
+
+    text: () => token.immediate(/[^#\[\]\[\]]+/),
+  },
 });
